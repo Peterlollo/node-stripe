fdefeb160174bcd8551d0d34c7511c12f735ec8e
Peter Lollo <peteralollo@gmail.com>
2016-04-28 11:55:35 -0700

    Add ng directives to input form

--- client/app/home/home.js
+++ client/app/home/home.js
@@ -1,4 +1,9 @@
 angular.module('fromAtoB.home', [])
   .controller('HomeController', function($scope, $http, Locations){
     $scope.cities = Locations.getCities();
+    $scope.userTrip = {};
+    $scope.autoCompleteCities = function(){
+
+    }
+    $scope.selectMenu = {};
   });
