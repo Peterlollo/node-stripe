bde9a452826331a916c37b2cd81612911a2d5836
Peter Lollo <peteralollo@gmail.com>
2016-05-05 15:48:28 -0700

    Code clean up and inline documentation

--- client/app/app.js
+++ client/app/app.js
@@ -1,7 +1,7 @@
 angular.module('fromAtoB', ['fromAtoB.home', 'fromAtoB.submitted', 'fromAtoB.services', 'ngRoute', 'ngMaterial', 'ngMessages'])
   .config(['$routeProvider', function($routeProvider) {
     $routeProvider
-    //Set up Appropriate routes here
+    //FRONT END ROUTES
       .when('/', {
         templateUrl: 'app/home/home.html',
         controller: 'HomeController'
@@ -14,6 +14,4 @@ angular.module('fromAtoB', ['fromAtoB.home', 'fromAtoB.submitted', 'fromAtoB.ser
         templateUrl: 'app/submitted/submitted.html',
         controller: 'SubmittedController'
       });
-      //Require and add $httpProvider interceptor here if needed
   }]);
-  //Add authorization checks here if needed
