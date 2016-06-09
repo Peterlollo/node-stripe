dac857f3ff616223d7cd1bbc305735c8f4ebcc85
Peter Lollo <peteralollo@gmail.com>
2016-06-09 10:08:29 -0700

    Rearrange js code

--- client/js/payment.js
+++ client/js/payment.js
@@ -1,47 +1,49 @@
 $(document).ready(function() {
     // Watch for a form submission:
     $("#payment-form").submit(function(event) {
+
         $('#submitBtn').attr('disabled', 'disabled');
-        return false;
+
+        var error = false;
+
+        // Get the values:
+        var ccNum = $('.card-number').val(),
+            cvcNum = $('.card-cvc').val(),
+            expMonth = $('.card-expiry-month').val(),
+            expYear = $('.card-expiry-year').val();
+
+        // Validate the number:
+        if (!Stripe.card.validateCardNumber(ccNum)) {
+            error = true;
+            reportError('The credit card number appears to be invalid.');
+        }
+
+        // Validate the CVC:
+        if (!Stripe.card.validateCVC(cvcNum)) {
+            error = true;
+            reportError('The CVC number appears to be invalid.');
+        }
+
+        // Validate the expiration:
+        if (!Stripe.card.validateExpiry(expMonth, expYear)) {
+            error = true;
+            reportError('The expiration date appears to be invalid.');
+        }
+
+        if (!error) {
+            // Get the Stripe token:
+            Stripe.card.createToken({
+                number: ccNum,
+                cvc: cvcNum,
+                exp_month: expMonth,
+                exp_year: expYear
+            }, stripeResponseHandler);
+         }
     }); // form submission
 }); // document ready.
 
 
-var error = false;
 
-// Get the values:
-var ccNum = $('.card-number').val(),
-    cvcNum = $('.card-cvc').val(),
-    expMonth = $('.card-expiry-month').val(),
-    expYear = $('.card-expiry-year').val();
-
-// Validate the number:
-if (!Stripe.card.validateCardNumber(ccNum)) {
-    error = true;
-    reportError('The credit card number appears to be invalid.');
-}
-
-// Validate the CVC:
-if (!Stripe.card.validateCVC(cvcNum)) {
-    error = true;
-    reportError('The CVC number appears to be invalid.');
-}
-
-// Validate the expiration:
-if (!Stripe.card.validateExpiry(expMonth, expYear)) {
-    error = true;
-    reportError('The expiration date appears to be invalid.');
-}
-
-if (!error) {
-    // Get the Stripe token:
-    Stripe.card.createToken({
-        number: ccNum,
-        cvc: cvcNum,
-        exp_month: expMonth,
-        exp_year: expYear
-    }, stripeResponseHandler);
- }
 
  function stripeResponseHandler(status, response) {
   if (response.error) {
