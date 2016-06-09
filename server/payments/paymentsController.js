887ebb145a35da9adc696fa663b3ded7b88f0246
Peter Lollo <peteralollo@gmail.com>
2016-06-09 10:27:53 -0700

    Resend index after payment token sent

--- server/payments/paymentsController.js
+++ server/payments/paymentsController.js
@@ -4,7 +4,8 @@ module.exports = {
   printPayment: function(req, res, next) {
     //SEND USER TRIP INFO TO DATABASE
     //PROCESS AND BOOK USER TRIP
-    console.log('THE REQUEST=====>>>>>>>', req);
+    console.log('THE REQUEST=====>>>>>>>', req.body);
+    res.sendFile(path.join(__dirname + '/index.html'));
     res.json(req.body);
     res.send(200);
   }
