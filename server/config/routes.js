bcfc31361ee385bc47ce75ddfdb3e65fab5ed32c
Peter Lollo <peteralollo@gmail.com>
2016-06-09 09:55:21 -0700

    Require payments controller in routes

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,5 +1,6 @@
 var citiesController = require('../cities/citiesController.js');
-var tripsController = require('../trips/tripsController.js')
+var tripsController = require('../trips/tripsController.js');
+var paymentsController = require('../payments/paymentsController.js');
 
 module.exports = function(app, express) {
   app.get('/api/locations/', citiesController.getCities);
